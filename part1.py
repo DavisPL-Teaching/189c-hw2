6d88580896a7cf030073248d1d0b4176f860b1d9
Caleb Stanford <cdstanford@ucdavis.edu>
2024-04-19 19:54:40 -0700

    :wrench: part 1 updates

--- part1.py
+++ part1.py
@@ -3,26 +3,38 @@ Part 1: Mini exercises
 """
 
 import z3
+import pytest
 
 """
 Helper functions
 
-You can use these instead of
+We will use these instead of
 z3.prove and z3.solve.
 The main difference between z3.prove and
 z3.solve is that they also return the result.
 """
 
+# Constants -- output of solve()
+SAT = z3.sat
+UNSAT = z3.unsat
+UNKNOWN = z3.unknown
+
+# Constants -- output of prove()
+PROVED = UNSAT
+FAILED = UNKNOWN
+COUNTEREXAMPLE = SAT
+
 def prove(spec):
     solver = z3.Solver()
     solver.add(z3.Not(spec))
     result = solver.check()
-    if result == z3.unsat:
+    if result == UNSAT:
         print("proved")
-    elif result == z3.unknown:
+    elif result == UNKNOWN:
         print("failed to prove")
         print(s.model())
     else:
+        # result == SAT
         print("counterexample")
         print(s.model())
     return result
@@ -31,11 +43,12 @@ def solve(spec):
     solver = z3.Solver()
     solver.add(spec)
     result = solver.check()
-    if result == z3.unsat:
+    if result == UNSAT:
         print("no solution")
-    elif result == z3.unknown:
+    elif result == UNKNOWN:
         print("failed to solve")
     else:
+        # result == SAT
         print("solution found")
         print(s.model())
     return result
@@ -44,38 +57,71 @@ def solve(spec):
 A. Writing specifications
 
 Consider the absolute value function
-that we discussed in lecture. Write a specification
-for the following properties, and use Z3 to check
-which of them hold.
-The first one is written for you.
+that we discussed in lecture:
 """
 
 def abs(x):
     return z3.If(x >= 0, x, -x)
 
 """
+Write a specification for the following properties,
+and use Z3 to check which of them hold.
+The first one is written for you.
+
+You can use the PROVED, FAILED, and COUNTEREXAMPLE
+constants for assertions in your tests.
+
 1. If x is greater than or equal to 0, then the absolute value of x is equal to x.
 
 2. If x is less than y, then the absolute value of x is less than the absolute value of y.
 
-4. If x is equal to 2 * y, then the absolute value of x is equal to 2 times the absolute value of y.
+4. If x is equal to y + 1, then the absolute value of x is equal to 1 plus the absolute value of y.
 
 5. The absolute value of the absolute value of x is equal to the absolute value of -x.
 
 6. The absolute value of x + y is less than or equal to the absolute value of x + the absolute value of y.
 """
 
+def test_abs_1():
+    x = z3.Int('x')
+    spec = z3.Implies(x > 0, abs(x) == x)
+    assert prove(spec) == PROVED
+
+def test_abs_2():
+    # TODO
+    raise NotImplementedError
+
+def test_abs_3():
+    # TODO
+    raise NotImplementedError
+
+def test_abs_4():
+    # TODO
+    raise NotImplementedError
+
+def test_abs_5():
+    # TODO
+    raise NotImplementedError
+
+def test_abs_6():
+    # TODO
+    raise NotImplementedError
+
 """
 B. Rectangle collision calculator
 
 Let's write a function that is able to calculate
-whether two rectangles collide. The rectangles
-are given by a width, height, position, and velocity.
+whether two rectangles collide.
+Initially, the two rectangles
+are given by a width, height, position of the center
+(in (x, y) coordinates), and velocity (in (vx, vy) coordinates).
+Over time, they move in the direction of the velocity every second.
 
-7. Write the function rectangle_position that calculates
-the position of a rectangle at a given time, as a Z3 expression.
+7. Write a function rectangle_position that calculates
+the position of a rectangle at a given time t,
+as a Z3 expression.
 
-8. Write the function rectangles_overlap that creates a formula whether
+8. Write a function rectangles_overlap that creates a formula whether
 two rectangles overlap at a specific time. The formula
 should be satisfiable if the two rectangles overlap and
 unsatisfiable if they do not overlap.
@@ -84,7 +130,7 @@ Hint: You can create two new variables,
     overlap_point_y
 that describe the point of overlap between the two rectangles.
 
-9. Write the function rectangles_collide that checks whether
+9. Write a function rectangles_collide that checks whether
 two rectangles collide at any point in time.
 The time is not given as an argument, because it should be
 included in the expressions for the rectangle's position.
@@ -93,8 +139,7 @@ Your function should call s3.solve to check whether
 there exists a point in time (t) and an overlap point
 (overlap_point_x, overlap_point_y)
 such that the two rectangles collide.
-
-10. Write two unit tests for rectangles_collide.
+You can use the constants SAT and UNSAT to check the output.
 """
 
 def rectangle_position(x, y, width, height, vx, vy, t):
@@ -102,8 +147,9 @@ def rectangle_position(x, y, width, height, vx, vy, t):
     x, y, width, height, vx, vy: Python integers
     t: a Z3 real number
 
-    returns: a tuple of Z3 expressions
-    that represents the position of the rectangle at time t.
+    returns: a tuple of two Z3 expressions
+    that represents the (x, y)
+    position of the center of the rectangle at time t.
     """
     # TODO
     raise NotImplementedError
@@ -133,6 +179,11 @@ def rectangles_collide(x1, y1, width1, height1, vx1, vy1,
     # TODO
     raise NotImplementedError
 
+"""
+10. Write two unit tests for rectangles_collide to
+check if it seems to be working.
+"""
+
 @pytest.mark.skip
 def test_rectangles_collide_1():
     # TODO
@@ -142,3 +193,10 @@ def test_rectangles_collide_1():
 def test_rectangles_collide_2():
     # TODO
     raise NotImplementedError
+
+"""
+11. Do you think this is the best way to check for collisions in general?
+For example, for collision detection in a game?
+What about for a simple prototype?
+Discuss one benefit and one drawback of this approach.
+"""
